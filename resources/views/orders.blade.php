7c2da0c136e8d464a0c6d9f0f93857fcf857b4b8
insssomniac <insssomniac@gmail.com>
2020-07-04 00:00:48 +0300

    Update orders.blade.php

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -23,7 +23,7 @@
                         <div class="cart-product-list__item">
                             <div class="cart-product__item__product-photo"><img src="/images/uploads/{{$product->image}}" class="cart-product__item__product-photo__image"></div>
                             <div class="cart-product__item__product-name">
-                                <div class="cart-product__item__product-name__content"><a href="#">{{$product->name}}</a></div>
+                                <div class="cart-product__item__product-name__content"><a href="/product/{{$product->id}}">{{$product->name}}</a></div>
                             </div>
                             <div class="cart-product__item__cart-date">
                                 <div class="cart-product__item__cart-date__content">{{$order->created_at}}</div>
